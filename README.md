ca2be92ad4bda48d0af2c6f4adb3c5383cb069a3
az13js <1654602334@qq.com>
2020-06-24 22:50:02 +0800

    数据库查询操作,调整代码

--- README.md
+++ README.md
@@ -1,3 +1,45 @@
 # Just CURD
 
-创建新的数据库`CREATE DATABASE test_justcurd CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;`。
+这是一个通过提供简单的 HTTP 请求，就能实现数据 CURD 操作的 PHP 程序。目前程序仅支持 MySQL, MariaDB。
+
+## 使用方法
+
+使用前需要进行准备：
+
+1. 创建新的数据库
+
+    CREATE DATABASE test_justcurd CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+2. 配置`index.php`文件。
+
+正式使用：
+
+### 查看全部表
+
+    curl http://127.0.0.1
+
+返回所有表
+
+    ["user"]
+
+### 查看`user`表的数据
+
+    curl http://127.0.0.1/user
+
+返回
+
+    [{"id":"1","name":"asdsada"}]
+
+### 查看不存在的表
+
+会在数据库新建那张表
+
+    curl http://127.0.0.1/school
+
+然后
+
+    curl http://127.0.0.1
+
+表变成了
+
+    ["school","user"]
--- index.php
+++ index.php
@@ -1,22 +1,58 @@
 <?php
+$host = '127.0.0.1';
+$dbname = 'test_justcurd';
+$port = '3306';
+$user = 'root';
+$password = '11111111';
+
 // 按照不同的请求方法，需要进行不同的操作
 switch ($_SERVER['REQUEST_METHOD']) {
     case 'GET':
+        header('Content-Type: application/json;charset=UTF-8');
         $location = strstr($_SERVER['REQUEST_URI'], '?', true);
         $resource = $location ? $location : $_SERVER['REQUEST_URI'];
-        if ('/' == $resource) {
-            header('Content-Type: text/plain; charset=UTF-8');
-            echo '欢迎使用。';
-        } else {
-            // 连接数据库，然后下面要对数据库进行操作
-            $database = new PDO('mysql:host=127.0.0.1;dbname=test_justcurd;port=3306;charset=utf8mb4', 'root', '11111111');
-            // 用完数据库后关闭连接
+        if ('/' == $resource || empty($resource)) {
+            try {
+                $database = new PDO("mysql:host=$host;dbname=$dbname;port=$port;charset=utf8mb4", $user, $password);
+            } catch (PDOException $e) {
+                header($_SERVER['SERVER_PROTOCOL'] . ' 503 Service Unavailable', 503);
+                echo json_encode($e->getMessage());
+                exit();
+            }
+            $result = $database->query('SHOW TABLES');
             $database = null;
+            if (false === $result) {
+                header($_SERVER['SERVER_PROTOCOL'] . ' 503 Service Unavailable', 503);
+                echo json_encode('获取所有的数据库表名失败！');
+            } else {
+                echo json_encode($result->fetchAll(PDO::FETCH_COLUMN));
+            }
+        } elseif (substr($resource, 0, 1) != '/') {
+            header($_SERVER['SERVER_PROTOCOL'] . ' 400 Bad Request', 400);
+            echo json_encode('请求的资源必须以“/”开头！');
+        } else {
+            $path = explode('/', $resource);
+            try {
+                $database = new PDO("mysql:host=$host;dbname=$dbname;port=$port;charset=utf8mb4", $user, $password);
+            } catch (PDOException $e) {
+                header($_SERVER['SERVER_PROTOCOL'] . ' 503 Service Unavailable', 503);
+                echo json_encode($e->getMessage());
+                exit();
+            }
+            $database->exec('CREATE TABLE IF NOT EXISTS `' . addslashes($path[1]) . '` (`id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci');
+            $result = $database->query('SELECT * FROM `' . addslashes($path[1]) . '`');
+            $database = null;
+            if (false === $result) {
+                header($_SERVER['SERVER_PROTOCOL'] . ' 503 Service Unavailable', 503);
+                echo json_encode('获取表数据失败！');
+            } else {
+                echo json_encode($result->fetchAll(PDO::FETCH_CLASS));
+            }
         }
         break;
     default:
-        header('Content-Type: text/plain; charset=UTF-8');
+        header('Content-Type: application/json;charset=UTF-8');
         header($_SERVER['SERVER_PROTOCOL'] . ' 405 Method Not Allowed', 405);
-        echo '请求方法：“' . $_SERVER['REQUEST_METHOD'] . '”目前不被服务器程序支持。';
+        echo json_encode('请求方法：“' . $_SERVER['REQUEST_METHOD'] . '”目前不被服务器程序支持。');
 }
 
